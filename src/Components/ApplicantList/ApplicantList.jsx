21f74be4bdaae3beec9d83c46d267844364a23eb
naomiuwa <naomi.uwamungu@gmail.com>
2023-08-03 08:15:22 +0100

    working on making the state functional in ApplicantList

--- src/Components/ApplicantList/ApplicantList.jsx
+++ src/Components/ApplicantList/ApplicantList.jsx
@@ -1,10 +1,15 @@
-import './ApplicantList.css';
+import "./ApplicantList.css";
 
 //this component receives candidateName and setCandidateName as props using destructuring.
 function ApplicantList({ candidateName, setCandidateName }) {
   return (
-    <div >
-      <h3>New Applicants</h3>
+    <div>
+      <input
+        type="text"
+        value={candidateName}
+        onChange={(event) => setCandidateName(event.target.value)}
+        placeholder="Enter candidate's name"
+      />
     </div>
   );
 }
